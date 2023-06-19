7e949e19c5b99a891c7477aa8ccebaf262414ad5
@a <28899947+astinz@users.noreply.github.com>
2023-06-19 09:03:28 +0300

    dirty...

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Sui Entity Component System...");
+use std::str::FromStr;
+use sui_sdk::types::base_types::SuiAddress;
+use sui_sdk::{SuiClientBuilder};
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let sui = SuiClientBuilder::default().build(
+        "https://fullnode.devnet.sui.io:443",
+    ).await.unwrap();
+    let rgp = sui.read_api().get_reference_gas_price().await?;
+    println!("{:?}", rgp);
+    Ok(())
 }
